e83594c2231a0d3716af4d247986990a262aa2fa
omeruysal81 <157880761+omeruysal81@users.noreply.github.com>
2024-08-24 10:30:52 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.forever(function () {
+    basic.showString("My")
+    basic.showIcon(IconNames.Giraffe)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+})
